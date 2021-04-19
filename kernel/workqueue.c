54296df87f18c463b5c6589602a1f0783dd86829
Zqiang <qiang.zhang@wilb@semihalf.com>
2021-04-19 15:57:41 -0400

    workqueue: Move the position of debug_work_activate

--- kernel/workqueue.c
+++ kernel/workqueue.c
@@ -1407,8 +1407,6 @@ static void __queue_work(int cpu, struct workqueue_struct *wq,
 	 */
 	WARN_ON_ONCE(!irqs_disabled());
 
-	debug_work_activate(work);
-
 	/* if draining, only works from the same workqueue are allowed */
 	if (unlikely(wq->flags & __WQ_DRAINING) &&
 	    WARN_ON_ONCE(!is_chained_work(wq)))
@@ -1490,6 +1488,7 @@ retry:
 		worklist = &pwq->delayed_works;
 	}
 
+	debug_work_activate(work);
 	insert_work(pwq, work, worklist, work_flags);
 
 	spin_unlock(&pwq->pool->lock);
